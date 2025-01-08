a8ced6b9f106762e1f33ac2287758e08658a14b7
palavi-822000 <palavikumari828@gmail.com>
2025-01-08 17:14:33 +0530

    form Validation added

--- src/main/java/in/pallavi/controller/copy/ProductController.java
+++ src/main/java/in/pallavi/controller/copy/ProductController.java
@@ -2,10 +2,12 @@ package in.pallavi.controller.copy;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import in.pallavi.binding.Product;
+import jakarta.validation.Valid;
 
 @Controller
 public class ProductController {
@@ -18,12 +20,19 @@ public class ProductController {
 	}
 	//method to handle the form submission
 	@PostMapping("/product")
-	public String handleSubmitBtn(Product p,Model model) {
-		System.out.println(p);
+	public String handleSubmitBtn(@Valid Product p, BindingResult result, Model model) {
 		
-		model.addAttribute("msg", "Product Saved!!");
-		
+		if(result.hasErrors()) {
+			return "index";
+			
+		}else{
+			model.addAttribute("msg", "Product Saved");
+			
+		}
 		return "index";
+		
+		
+		
 		
 	}
 }
